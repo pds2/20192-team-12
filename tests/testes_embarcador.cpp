403c6c7732391e9049173198fed2cda8e08cfcd8
weabreu <abreu.we@gmail.com>
2019-11-02 19:50:12 -0300

    Alteracao de testes de unidade.

--- tests/testes_embarcador.cpp
+++ tests/testes_embarcador.cpp
@@ -12,18 +12,18 @@ TEST_CASE("01 - Testes de contorno") {
     Embarcador e(1000, 1, 2);
 
     CHECK_NOTHROW(e.setOrigem(-1));
-	CHECK_NOTHROW(e.setDestino(-1));
-	CHECK_NOTHROW(e.setQuantidade(-1));
+    CHECK_NOTHROW(e.setDestino(-1));
+    CHECK_NOTHROW(e.setQuantidade(-1));
 }
-TEST_CASE("02 - Testando setters e getters"){
+// TEST_CASE("02 - Testando setters e getters"){
 
-	Embarcador e(1000, 1, 2);
+//     Embarcador e(1000, 1, 2);
 
-	e.setQuantidade(100);
-	e.setOrigem(2);
-	e.setDestino(3);
+//     e.setQuantidade(100);
+//     e.setOrigem(2);
+//     e.setDestino(3);
 
-	CHECK(e.getQuantidade() == 100);
-	CHECK(e.getOrigem() == 2);
-	CHECK(e.getDestino() == 3);
-}
+//     CHECK(e.getQuantidade() == 100);
+//     CHECK(e.getOrigem() == 2);
+//     CHECK(e.getDestino() == 3);
+// }
