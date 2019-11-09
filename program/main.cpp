7a66fcafc74e9c7a1a7dad7e87e77a921fe7d1ee
Wagner <abreu.we@gmail.com>
2019-11-08 22:43:34 -0300

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -19,9 +19,6 @@ int main(){
     std::string linha;
     std::vector <std::string> vec_local; // matriz para armazenar os dados de entrada
     std::vector <std::string> vec_arestas; // matriz para armazenar os dados de entrada
-    // std::string nome_usr, senha;
-    // Usuario usr = new Usuario(nome, );
-    // obtem o nome do arquivo do argumento e abre o arquivo
     std::string caminho = "./data/";
     std::ifstream arq_local(caminho + "localidades.txt");
     std::ifstream arq_arestas(caminho + "arestas.txt");
@@ -38,23 +35,6 @@ int main(){
     std::cout << "int: " << tipo.getTipo(origem) << std::endl;
     // std::string nome, login, senha;
     // Usuario *usr = new Usuario;
-    /*
-
-    */
-    /*
-    ENTRADA DO USUARIO
-    std::cout << "Digite o login do usuario: ";
-    std::cout << std::endl;
-    std::cout << "Digite a senha: ";
-    std::cin >> senha;
-    std::cout << std::endl;
-    usr->setNome(nome);
-    usr->setLogin(login);
-    usr->setSenha(senha);
-    usr->cadastrarUsuario();
-    std::cout << (caminho + "localidades.txt") << std::endl;
-    std::cout << (caminho + "arestas.txt") << std::endl;
-    */
     /*
         LEITURA DOS ARQUIVOS
     */   
@@ -74,20 +54,5 @@ int main(){
     std::cout << std::endl;
     arq_local.close();
     arq_arestas.close();
-    // std::ofstream file;
-    // file.open(caminho + "localidades.txt");
-    // file.seekp (0, file.end);
-    // std::string str = std::to_string(41) + "|TESTE|" + "AUS" + "|AUSTRALIA|" +  std::to_string(-33.875935) +  std::to_string(151.251827)+"\n";
-    // file <<  str;
-
-
-        // Rodoviario *r = new Rodoviario();
-        // std::cout << "Pedagio: " << r->getValorPedagio() << "\n";
-        // // delete r;
-    // delete usr;
-    // std::cout << vec_local.size() << "\n";
-    // for (unsigned int i = 0; i < vec_local.size(); i++){
-    //     std::cout << vec_local[i] << "\n";
-    // }
     return 1;
 }
