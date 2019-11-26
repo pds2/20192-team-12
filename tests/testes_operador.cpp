70580c9bc258ef612a7318b853e0f18f2abf4151
weabreu <abreu.we@gmail.com>
2019-11-25 21:50:14 -0300

    Atualizando as pastas tests, program, src e include.cpp

--- tests/testes_operador.cpp
+++ tests/testes_operador.cpp
@@ -31,6 +31,27 @@ TEST_CASE("02 - Testes positivos"){
     lerArquivoLocalidades(arq_local, vec_local);
 
     Operador o = Operador(vec_local.size());
-	CHECK_NOTHROW(o.addSolicitacao(2, 3, 100));
-	CHECK_NOTHROW(o.dijkstra(2, 3, 100, vec_local));
+	CHECK_NOTHROW(o.addSolicitacao(0,1,100));
+    CHECK_NOTHROW(o.addSolicitacao(4,5,1));
+    CHECK_NOTHROW(o.addSolicitacao(6,7,35));
+    CHECK_NOTHROW(o.addSolicitacao(8,9,36));
+    CHECK_NOTHROW(o.addSolicitacao(10,11,50));
+    CHECK_NOTHROW(o.addSolicitacao(12,13,70));
+    CHECK_NOTHROW(o.addSolicitacao(14,15,80));
+    CHECK_NOTHROW(o.addSolicitacao(16,17,90));
+    CHECK_NOTHROW(o.addSolicitacao(18,19,100));
+    CHECK_NOTHROW(o.addSolicitacao(20,21,110));
+    CHECK_NOTHROW(o.addSolicitacao(22,23,120));
+    CHECK_NOTHROW(o.addSolicitacao(2,3,100000));
+
+    CHECK_NOTHROW(o.dijkstra(2,3,1, vec_local));
+    CHECK_NOTHROW(o.dijkstra(4,7,10, vec_local));
+    CHECK_NOTHROW(o.dijkstra(6,11,100, vec_local));
+    CHECK_NOTHROW(o.dijkstra(8,15,200, vec_local));
+    CHECK_NOTHROW(o.dijkstra(10,19,1000, vec_local));
+    CHECK_NOTHROW(o.dijkstra(12,23,2000, vec_local));
+    CHECK_NOTHROW(o.dijkstra(14,27,10000, vec_local));
+    CHECK_NOTHROW(o.dijkstra(16,31,50000, vec_local));
+    CHECK_NOTHROW(o.dijkstra(18,35,100000, vec_local));
+
 }
