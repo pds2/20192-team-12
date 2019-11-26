58eefdea5ea70e14c0e7721370cadb0e5f6dd486
weabreu <abreu.we@gmail.com>
2019-11-26 13:34:51 -0300

    Atualizando e removendo arquivos.

--- tests/testes_operador.cpp
+++ tests/testes_operador.cpp
@@ -7,14 +7,26 @@ TEST_CASE("01 - Testes de contorno") {
     CHECK_THROWS(Operador(0)); // argumento zero
     CHECK_THROWS(Operador(-1)); // argumento negativo
 
-    Operador operador0 = Operador(-1);// argumento negativo
 
-    std::vector <Localidade> vec_local;
-    Operador operador1 = Operador(vec_local.size());// argumento negativo
-    
-	CHECK_THROWS(operador1.popularMatriz());
+    // Testando argumentos negativos e iguais a zero
+    std::vector <Localidade> vec_local; 
+    std::ifstream arq_local("./data/localidades.txt");
+    lerArquivoLocalidades(arq_local, vec_local);
+    Operador operador1 = Operador(vec_local.size());
+
+	CHECK_NOTHROW(operador1.popularMatriz());
     CHECK_THROWS(operador1.dijkstra(-1, -1, -1, vec_local)); // argumentos negativo
     CHECK_THROWS(operador1.dijkstra(0, 0, 0, vec_local)); // argumentos com valor zero
+    CHECK_THROWS(operador1.addSolicitacao(-1, -1, -1)); // argumentos negativo
+    CHECK_THROWS(operador1.addSolicitacao(0, 0, 0)); // argumentos com valor zero
+
+    // Testando vetor vazio
+    std::vector <Localidade> vetor_vazio; 
+
+    CHECK_THROWS(Operador(vetor_vazio.size()));
+	CHECK_NOTHROW(operador1.popularMatriz());
+    CHECK_THROWS(operador1.dijkstra(-1, -1, -1, vetor_vazio)); // argumentos negativo
+    CHECK_THROWS(operador1.dijkstra(0, 0, 0, vetor_vazio)); // argumentos com valor zero
     CHECK_THROWS(operador1.addSolicitacao(-1, -1, -1)); // argumentos negativo
     CHECK_THROWS(operador1.addSolicitacao(0, 0, 0)); // argumentos com valor zero
 
@@ -24,8 +36,8 @@ TEST_CASE("02 - Testes positivos"){
     std::vector <Localidade> vec_local; 
     std::ifstream arq_local("./data/localidades.txt");
     lerArquivoLocalidades(arq_local, vec_local);
-
     Operador o = Operador(vec_local.size());
+
 	CHECK_NOTHROW(o.addSolicitacao(0,1,100));
     CHECK_NOTHROW(o.addSolicitacao(4,5,1));
     CHECK_NOTHROW(o.addSolicitacao(6,7,35));
--- tests/testes_solicitacao.cpp
+++ tests/testes_solicitacao.cpp
@@ -1,17 +1,21 @@
+#include <vector>
 #include  "../third_party/doctest.h"
 #include "../include/solicitacao.hpp"
+#include "../include/localidade.hpp"
+#include "../include/tools.hpp"
 
 TEST_CASE("01 - Testes de contorno") {
     CHECK_NOTHROW(Solicitacao()); // argumento vazio
-    CHECK_THROWS(Solicitacao(-1, 1, 2)); // argumento negativo
+    CHECK_THROWS(Solicitacao(-1, -1, 2)); // argumento negativo
 
     Solicitacao s(1000, 1, 2);
 
-    CHECK_THROWS(s.setOrigem(-1));
-    CHECK_THROWS(s.setDestino(-1));
-    CHECK_THROWS(s.setQuantidade(-1));
+    CHECK_THROWS(s.setOrigem(-1)); // argumento negativo
+    CHECK_THROWS(s.setDestino(-1));// argumento negativo
+    CHECK_THROWS(s.setQuantidade(-1));// argumento negativo
+
 }
-TEST_CASE("02 - Testando setters e getters"){
+TEST_CASE("03 - Testando setters e getters"){
     // Teste positivo
     Solicitacao s(1000, 1, 2);
 
